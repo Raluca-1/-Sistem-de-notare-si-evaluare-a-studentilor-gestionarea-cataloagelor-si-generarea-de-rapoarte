5261591fe83cc92d6c558155089ed638b2c0331f
Raluca-1 <mraluca464@gmail.com>
2026-01-06 20:28:32 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,245 +4,245 @@
 
 using namespace std;
 
-void afiseazaMeniuAdministrator() {
-    cout << "\n\nMeniu Administrator:\n\n";
-    cout << "1. Afiseaza catalog\n";
-    cout << "2. Adauga student\n";
-    cout << "3. Sterge student\n";
-    cout << "4. Afiseaza restantieri\n";
-    cout << "5. Afiseaza bursieri\n";
-    cout << "6. Iesire\n";
+void displayAdminMenu() {
+    cout << "\n\nAdministrator Menu:\n\n";
+    cout << "1. Display catalog\n";
+    cout << "2. Add student\n";
+    cout << "3. Remove student\n";
+    cout << "4. Display failing students\n";
+    cout << "5. Display scholarship students\n";
+    cout << "6. Exit\n";
 }
 
-void afiseazaMeniuProfesor() {
-    cout << "\n\nMeniu Profesor:\n\n";
-    cout << "1. Afiseaza catalog\n";
-    cout << "2. Cauta student\n";
-    cout << "3. Adauga nota\n";
-    cout << "4. Afisare restantieri\n";
-    cout << "5. Iesire\n";
+void displayProfessorMenu() {
+    cout << "\n\nProfessor Menu:\n\n";
+    cout << "1. Display catalog\n";
+    cout << "2. Search student\n";
+    cout << "3. Add nota\n";
+    cout << "4. Display failing students\n";
+    cout << "5. Exit\n";
 }
 
-void meniuStudent() {
-    cout << "\n\nMeniu Student:\n\n";
-    cout << "1. Afisare note\n";
-    cout << "2. Afisare medie generala\n";
-    cout << "3. Verificare restante\n";
-    cout << "4. Verificare bursier\n";
-    cout << "5. Iesire\n";
+void displayStudentMenu() {
+    cout << "\n\nStudent Menu:\n\n";
+    cout << "1. Display grades\n";
+    cout << "2. Display average\n";
+    cout << "3. Check failing status\n";
+    cout << "4. Check scholarship status\n";
+    cout << "5. Exit\n";
 }
 
 int main() {
     Catalog catalog;
-    catalog.initStudenti(); // catalogul cu 10 studenti
+    catalog.initStudents(); // Catalogul cu 10 studenti
 
-    string rol;
-    cout << "Cum va intitulati? (administrator/profesor/student): ";
-    cin >> rol;
+    string role;
+    cout << "What is your role? (administrator/professor/student): ";
+    cin >> role;
 
-    if (rol == "administrator") {
-        int optiune;
+    if (role == "administrator") {
+        int option;
         do {
-            afiseazaMeniuAdministrator();
-            cout << "Alege o optiune: ";
-            cin >> optiune;
+            displayAdminMenu();
+            cout << "Choose an option: ";
+            cin >> option;
 
-            switch (optiune) {
+            switch (option) {
             case 1:
-                catalog.afiseazaCatalog();
+                catalog.displayCatalog();
                 break;
             case 2: {
-                string nume;
-                int grupa;
-                char semigrupa;
+                string name;
+                int group;
+                char subgroup;
 
-                cout << "Introduceti numele studentului: ";
-                cin.ignore();  // Curata orice caracter ramas în buffer-ul de input (daca exista)
-                getline(cin, nume);  // Citire intreaga linie (inclusiv spatiile)
+                cout << "Enter student's name: ";
+                cin.ignore();  // Curata orice caracter ramas in buffer-ul de input (daca exista)
+                getline(cin, name);  // Citire intreaga linie (inclusiv spatiile)
 
 
-                cout << "Introduceti grupa studentului: ";
-                cin >> grupa;
+                cout << "Enter group's student: ";
+                cin >> group;
 
-                cout << "Introduceti semigrupa studentului: ";
-                cin >> semigrupa;
+                cout << "Enter subgroup's student: ";
+                cin >> subgroup;
 
-                Student student(nume, grupa, semigrupa);
-                catalog.adaugaStudent(student);
+                Student student(name, group, subgroup);
+                catalog.addStudent(student);
                 break;
             }
             case 3: {
                 int id;
-                cout << "Introduceti ID-ul studentului pe care doriti sa il stergeti: ";
+                cout << "Enter the ID of the student you want to remove: ";
                 cin >> id;
-                catalog.stergeStudent(id);
+                catalog.deleteStudent(id);
                 break;
             }
             case 4:
-                catalog.afiseazaRestantieri();
+                catalog.displayFailingStudents();
                 break;
             case 5:
-                catalog.afiseazaBursieri();
+                catalog.displayScholarshipStudents();
                 break;
             case 6:
-                cout << "Iesire...\n";
+                cout << "Exit...\n";
                 break;
             default:
-                cout << "Optiune invalida!\n";
+                cout << "Invalid option!\n";
             }
-        } while (optiune != 7);
+        } while (option != 6);
     }
-    else if (rol == "profesor") {
-        int optiune;
+    else if (role == "professor") {
+        int option;
         do {
-            afiseazaMeniuProfesor();
-            cout << "Alege o optiune: ";
-            cin >> optiune;
+            displayProfessorMenu();
+            cout << "Choose an option: ";
+            cin >> option;
 
-            switch (optiune) {
+            switch (option) {
             case 1:
-                catalog.afiseazaCatalog();
+                catalog.displayCatalog();
                 break;
             case 2: {
-                string nume;
+                string name;
 
-                cout << "Introduceti numele studentului pe care doriti sa il cautati: ";
-                cin.ignore();  // Curata orice caracter ramas în buffer-ul de input (daca exista)
-                getline(cin, nume);  // Citire intreaga linie (inclusiv spatiile)
+                cout << "Enter the name of the student you want to search: ";
+                cin.ignore();  // Curata orice caracter ramas in buffer-ul de input (daca exista)
+                getline(cin, name);  // Citire intreaga linie (inclusiv spatiile)
 
 
-                Student* student = catalog.cautaStudentDupaNume(nume);
+                Student* student = catalog.searchStudentByName(name);
                 if (student != nullptr) {
-                    cout << "Student gasit: " << student->getNume() << endl;
+                    cout << "Student found: " << student->getName() << endl;
                 }
                 else {
-                    cout << "Studentul nu a fost gasit.\n";
+                    cout << "Student not found.\n";
                 }
                 break;
             }
             case 3: {
-                string nume;
-                int nota;
+                string name;
+                int grade;
 
-                cout << "Introduceti numele studentului: ";
-                cin.ignore();  // Curata orice caracter ramas în buffer-ul de input (daca exista)
-                getline(cin, nume);  // Citire întreaga linie (inclusiv spatiile)
+                cout << "Enter the name of the student: ";
+                cin.ignore();  // Curata orice caracter ramas in buffer-ul de input (daca exista)
+                getline(cin, name);  // Citire întreaga linie (inclusiv spatiile)
 
 
-                Student* student = catalog.cautaStudentDupaNume(nume);
+                Student* student = catalog.searchStudentByName(name);
                 if (student != nullptr) {
-                    cout << "Introduceti nota: ";
-                    cin >> nota;
-                    student->adaugaNota(nota);
-                    cout << "Nota adaugata.\n";
+                    cout << "Enter the grade: ";
+                    cin >> grade;
+                    student->addGrade(grade);
+                    cout << "Grade added.\n";
                 }
                 else {
-                    cout << "Studentul nu a fost gasit.\n";
+                    cout << "Student not found.\n";
                 }
                 break;
             }
             case 4:
-                catalog.afiseazaRestantieri();
+                catalog.displayFailingStudents();
                 break;
             case 5:
-                cout << "Iesire...\n";
+                cout << "Exit...\n";
                 break;
             default:
-                cout << "Optiune invalida!\n";
+                cout << "Invalid option!\n";
             }
-        } while (optiune != 6);
+        } while (option != 5);
     }
-    else if (rol == "student") {
-        int optiune;
+    else if (role == "student") {
+        int option;
         do {
-            meniuStudent();
-            cout << "Alege o optiune: ";
-            cin >> optiune;
+            displayStudentMenu();
+            cout << "Choose an option: ";
+            cin >> option;
 
-            switch (optiune) {
+            switch (option) {
             case 1: {
-                string nume;
+                string name;
 
-                cout << "Introduceti numele pentru a vizualiza notele: ";
-                cin.ignore();  // Curata orice caracter ramas în buffer-ul de input (daca exista)
-                getline(cin, nume);  // Citire intreaga linie (inclusiv spatiile)
+                cout << "Enter your name for checking the grades: ";
+                cin.ignore();  // Curata orice caracter ramas in buffer-ul de input (daca exista)
+                getline(cin, name);  // Citire intreaga linie (inclusiv spatiile)
 
 
-                Student* student = catalog.cautaStudentDupaNume(nume);
+                Student* student = catalog.searchStudentByName(name);
                 if (student != nullptr) {
-                    const auto& note = student->getNote();
-                    cout << "Notele studentului " << nume << ": ";
-                    for (int nota : note) {
-                        cout << nota << " ";
+                    const auto& grades = student->getGrades();
+                    cout << "Grades of student " << name << ": ";
+                    for (int grade : grades) {
+                        cout << grade << " ";
                     }
                     cout << endl;
                 }
                 else {
-                    cout << "Studentul nu a fost gasit.\n";
+                    cout << "Student not found.\n";
                 }
                 break;
             }
             case 2: {
-                string nume;
+                string name;
 
-                cout << "Introduceti numele pentru a vizualiza media generala: ";
-                cin.ignore();  // Curata orice caracter ramas în buffer-ul de input (daca exista)
-                getline(cin, nume);  // Citire întreaga linie (inclusiv spatiile)
+                cout << "Enter the name to view overall average: ";
+                cin.ignore();  // Curata orice caracter ramas in buffer-ul de input (daca exista)
+                getline(cin, name);  // Citire întreaga linie (inclusiv spatiile)
 
 
-                Student* student = catalog.cautaStudentDupaNume(nume);
+                Student* student = catalog.searchStudentByName(name);
                 if (student != nullptr) {
-                    cout << "Media studentului " << nume << ": " << student->medie() << endl;
+                    cout << "Student " << name << "'s overall average: " << student->average() << endl;
                 }
                 else {
-                    cout << "Studentul nu a fost gasit.\n";
+                    cout << "Student not found.\n";
                 }
                 break;
             }
             case 3: {
-                string nume;
+                string name;
 
-                cout << "Introduceti numele pentru verificare: ";
-                cin.ignore();  // Curata orice caracter ramas în buffer-ul de input (dacă există)
-                getline(cin, nume);  // Citere intreaga linie (inclusiv spatiile)
+                cout << "Enter your name to check failing status: ";
+                cin.ignore();  // Curata orice caracter ramas in buffer-ul de input (dacă există)
+                getline(cin, name);  // Citere intreaga linie (inclusiv spatiile)
 
 
-                Student* student = catalog.cautaStudentDupaNume(nume);
+                Student* student = catalog.searchStudentByName(name);
                 if (student != nullptr) {
-                    cout << "Studentul " << nume << (student->esteRestantier() ? " este restantier.\n" : " nu este restantier.\n");
+                    cout << "Student " << name << (student->isFailing() ? " is failing.\n" : " is not failing.\n");
                 }
                 else {
-                    cout << "Studentul nu a fost gasit.\n";
+                    cout << "Student not found.\n";
                 }
                 break;
             }
             case 4: {
-                string nume;
+                string name;
 
-                cout << "Introduceti numele pentru verificare: ";
-                cin.ignore();  // Curata orice caracter ramas în buffer-ul de input (daca exista)
-                getline(cin, nume);  // Citire intreaga linie (inclusiv spatiile)
+                cout << "Enter your name to check scholarship status: ";
+                cin.ignore();  // Curata orice caracter ramas in buffer-ul de input (daca exista)
+                getline(cin, name);  // Citire intreaga linie (inclusiv spatiile)
 
 
-                Student* student = catalog.cautaStudentDupaNume(nume);
+                Student* student = catalog.searchStudentByName(name);
                 if (student != nullptr) {
-                    cout << "Studentul " << nume << (student->esteBursier() ? " este bursier.\n" : " nu este bursier.\n");
+                    cout << "Student " << name << (student->isScholar() ? " is a scholarship student.\n" : " is not a scholarship student.\n");
                 }
                 else {
-                    cout << "Studentul nu a fost gasit.\n";
+                    cout << "Student not found.\n";
                 }
                 break;
             }
             case 5:
-                cout << "Iesire...\n";
+                cout << "Exit...\n";
                 break;
             default:
-                cout << "Optiune invalida!\n";
+                cout << "Invalid option!\n";
             }
-        } while (optiune != 5);
+        } while (option != 5);
     }
     else {
-        cout << "Rol invalid! Iesire...\n";
+        cout << "Invalid role! Exit...\n";
     }
 
     return 0;
